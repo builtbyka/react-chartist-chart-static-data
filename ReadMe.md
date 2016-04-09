90d1bb7a1aca9227f1cdc0f10ab824aacdf43f93
Kerri-anne Ellis <kerri-anne.ellis@imperial.ac.uk>
2016-04-09 15:42:42 +0100

    commit of static data graph

--- ReadMe.md
+++ ReadMe.md
@@ -1,8 +1,12 @@
-#React Clientside boilerplate
+#React charts with chartist - static data
 
 Run npm install to get all modules setup
 
 Run gulp dev to start developing with local server
+
+Includes a matrix table
+
+Instant gratification - chart updates on submit - no waiting for database
 
 
 
--- component_src/App.jsx
+++ component_src/App.jsx
@@ -2,89 +2,36 @@ import React from 'react';
 import MatrixInput from './MatrixInput.jsx';
 import ChartistGraph from 'react-chartist';
 import ChartistLegend from './ChartistLegend.jsx';
-import Fetch from 'isomorphic-fetch';
 
 
 class App extends React.Component {
-    
-     componentDidMount() {
-
-        fetch('http://main-1914118172.eu-west-1.elb.amazonaws.com/activities/charttest')
-            .then(function(response) {
-                if (response.status >= 400) {
-                    throw new Error('Bad response from server');
-                }
-                return response.json();
-            })
-            .then((results) => {
-                let series = [];
-                results.forEach(
-                    result =>{
-                        let counter = 0;
-                          for(let i in result){
-                             if(i !== 'questionID'){
-                                  if(series[counter] === undefined){
-                                      series.push([]);
-                                  }
-                                  series[counter].push(result[i]);
-                                  counter ++;
-                              }
-                          }
-                    }
-                )
-                this.setState({series : series})
-            });
-    }
-
-    componentWillUnmount() {
-        //this is fine but overkill for the mo.
-        //this.serverRequest.abort();
-    }
 
 	constructor(props){
 		super(props);
         this.state = {
-                    options : ['affirmative', 'cognitive'],
-                    inptype : 'radio',
-                    labels : ['date','movie','gift'],
-                    graphOps : {},
-                    type : 'Bar',
-                    answers : [],
-                    series: [],
-                    userID:'mwellss',
-                    instanceID:'TEST01',
-                    versionID:'0.1.1',
-                    timeStampUTC:'1457696167',
-                    ip:'1.1.1.1',
-                    enviroment:'Edx'
-                };
+            options : ['I love it', 'I hate it', 'I don\'t mind'],
+            inptype : 'radio',
+            labels : ['Marmite','Peanut butter','Jam','Nutella'],
+            series : [[1, 2, 4, 8],[3, 2, 5, 1],[1, 1, 1, 2]],
+            graphOps : {},
+            type : 'Bar',
+            answers : []
+        };
         this.updateSeries = this.updateSeries.bind(this);
         this.updateAnswers = this.updateAnswers.bind(this);
 	}
-  
+   
     
     updateSeries(e){
-            let seriesOption,
+        let seriesOption,
             seriesLabel,
-            seriesCopy = this.state.series.slice(0),
-            submission = [
-            ];
-        
-        //Update series and submission data
-        
+            seriesCopy = this.state.series.slice(0);
         this.state.answers.forEach(
             answer => {
-                let submissionSegment = {userID: this.state.userID,
-                instanceID:this.state.instanceID,
-                versionID:this.state.versionID,
-                enviroment:this.state.enviroment}
                 this.state.options.forEach(function(value, i){
                     if(value === answer.value){
                         seriesOption = i;
-                        submissionSegment[value] = 1;
-                    } else{
-                        submissionSegment[value] = 0;
-                    } 
+                    }   
                 })
                  this.state.labels.forEach(function(value, i){
                     if(value === answer.name){
@@ -92,28 +39,14 @@ class App extends React.Component {
                     }   
                 })
                  seriesCopy[seriesOption][seriesLabel] += 1;
-                 submissionSegment.questionID = answer.name;
-                 submission.push(submissionSegment);
             }
+            
         )
-        
-        //update the page
         
         this.setState({series: seriesCopy});
        
-        //send data
-        
-        fetch('http://main-1914118172.eu-west-1.elb.amazonaws.com/activities/charttest', {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(submission)
-        }).then(response => console.log(response))
-        
-      
     }
-  
+    
     updateAnswers(e){
         let name = e.currentTarget.name,
             value = e.currentTarget.value,
@@ -127,7 +60,7 @@ class App extends React.Component {
                     }
                 }
             )
-       
+        
         if(found === false){
              answersCopy.push({name:name, value:value});
         }
@@ -137,26 +70,19 @@ class App extends React.Component {
     }
 
 	render(){
+        var overflow = {
+            overflow: 'auto'
+        };
 		return (
 			<div>
+                <div style={overflow}>
                   <MatrixInput options={this.state.options} type={this.state.inptype} questions={this.state.labels} updateAnswers={this.updateAnswers} updateSeries={this.updateSeries}/>
-                  <div style={styles.chart}>
-                    <ChartistGraph data={this.state} options={this.state.graphOps} type={this.state.type} />
-                  </div>
-                  <ChartistLegend style="width: 20%" type={this.state.type} legend={this.state.options}/>
+                </div> 
+                  <ChartistGraph data={this.state} options={this.state.graphOps} type={this.state.type} />
+                  <ChartistLegend legend={this.state.options}/>
 			</div>
 		)
 	}
-}
-
-let styles = {
-    
-    chart : {
-        float: 'left',
-         marginRight: '2%',
-        width: '85%',
-    }
-    
 }
 
 export default App
